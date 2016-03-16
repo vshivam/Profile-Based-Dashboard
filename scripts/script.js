754b46322c2dbae7be6431cfef1ab987f21db88f
shivam <shivam.bitsgoa@gmail.com>
2016-03-16 17:59:48 +0800

    removed theme chooser listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
 
-	$("input:radio[name=theme-radio-btn]").click(function() {
-    	var theme_value = $(this).val();
-
-        switch(theme_value){
-            case "dark":
-                console.log("Setting dark theme");
-                $("#jquery-ui-stylesheet").attr({href : "https://code.jquery.com/ui/1.11.4/themes/ui-darkness/jquery-ui.css"});
-                $("#jqm-ui-stylesheet").attr({href : "css/dark-theme/dark-theme.min.css"});
-                $("#jqm-icon-stylesheet").attr({href : "css/dark-theme/jquery.mobile.icons.min.css"});
-                break;
-            case "light":
-                console.log("Setting light theme");
-                $("#jquery-ui-stylesheet").attr({href : "https://code.jquery.com/ui/1.11.4/themes/overcast/jquery-ui.css"});
-                $("#jqm-ui-stylesheet").attr({href : "css/light-theme/light-theme.min.css"});
-                $("#jqm-icon-stylesheet").attr({href : "css/light-theme/jquery.mobile.icons.min.css"});    
-                break;            
-        }
-	});
-
-    ScopeUtils.loadDataIntoView();
+	ScopeUtils.loadDataIntoView();
 
     $(document).on("pageshow","#controls-page",function(event){ 
         console.log("pageshow devices page");
